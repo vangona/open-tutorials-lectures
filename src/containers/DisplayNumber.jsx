b9f03b4debb205e702d0b4bb343b9cb5d553e4fa
vangona <rlarhksrud12@naver.com>
2022-05-29 01:36:51 +0900

    React Redux 6.3 mapStateToProps

--- src/containers/DisplayNumber.jsx
+++ src/containers/DisplayNumber.jsx
@@ -1,7 +1,18 @@
 import DisplayNumber from "../components/DisplayNumber";
 import { connect } from "react-redux";
+function mapReduxStateToReactProps(state) {
+  return {
+    number: state.number,
+  };
+}
+function mapReduxDispatchToReactProps() {
+  return;
+}
 
-export default connect()(DisplayNumber);
+export default connect(
+  mapReduxStateToReactProps,
+  mapReduxDispatchToReactProps
+)(DisplayNumber);
 
 // import React, { Component } from "react";
 // import store from "../store";
