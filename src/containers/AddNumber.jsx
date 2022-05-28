b5b5619d74e346232415b896b36fb55d778239c6
vangona <rlarhksrud12@naver.com>
2022-05-29 01:32:37 +0900

    React Redux 6.2 connect & provider

--- src/containers/AddNumber.jsx
+++ src/containers/AddNumber.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
+import { connect } from "react-redux";
 import AddNumber from "../components/AddNumber";
-import store from "../store";
 
-export default class extends Component {
-  render() {
-    return (
-      <AddNumber
-        onClick={function (size) {
-          store.dispatch({ type: "INCREMENT", size: size });
-        }.bind(this)}
-      ></AddNumber>
-    );
-  }
-}
+export default connect()(AddNumber);
+
+// import React, { Component } from "react";
+// import store from "../store";
+
+// export default class extends Component {
+//   render() {
+//     return (
+//       <AddNumber
+//         onClick={function (size) {
+//           store.dispatch({ type: "INCREMENT", size: size });
+//         }.bind(this)}
+//       ></AddNumber>
+//     );
+//   }
+// }
--- src/containers/DisplayNumber.jsx
+++ src/containers/DisplayNumber.jsx
@@ -1,18 +1,21 @@
-import React, { Component } from "react";
 import DisplayNumber from "../components/DisplayNumber";
-import store from "../store";
+import { connect } from "react-redux";
 
-export default class extends Component {
-  state = { number: store.getState().number };
-  constructor(props) {
-    super(props);
-    store.subscribe(
-      function () {
-        this.setState({ number: store.getState().number });
-      }.bind(this)
-    );
-  }
-  render() {
-    return <DisplayNumber number={this.state.number}></DisplayNumber>;
-  }
-}
+export default connect()(DisplayNumber);
+
+// import React, { Component } from "react";
+// import store from "../store";
+// export default class extends Component {
+//   state = { number: store.getState().number };
+//   constructor(props) {
+//     super(props);
+//     store.subscribe(
+//       function () {
+//         this.setState({ number: store.getState().number });
+//       }.bind(this)
+//     );
+//   }
+//   render() {
+//     return <DisplayNumber number={this.state.number}></DisplayNumber>;
+//   }
+// }
