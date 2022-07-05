6690ba3678c00cc600316d5f69edd8d6fb4b67fc
vangona <rlarhksrud12@naver.com>
2022-07-05 22:04:16 +0900

    Add: 7강. 수정

--- open-tutorials__lowdb/main.js
+++ open-tutorials__lowdb/main.js
@@ -4,18 +4,22 @@ const adapter = new FileSync("db.json");
 const db = low(adapter);
 
 // Set some defaults (jsonFile에 해당 것들이 없으면 데이터 타입에 맞춰서 생성해준다.)
+
+// 3강. 기본 데이터 설정
 db.defaults({ topic: [], author: [] }).write();
+
+// 4강. 생성
 // db.get("author")
 //   .push({
 //     id: 1,
-//     name: "egoing",
+//     title: "egoing",
 //     profile: "developer",
 //   })
 //   .write();
 // db.get("topic")
 //   .push({
 //     id: 1,
-//     name: "lowdb",
+//     title: "lowdb",
 //     description: "lowdb is ...",
 //     author: 1,
 //   })
@@ -30,4 +34,8 @@ db.defaults({ topic: [], author: [] }).write();
 //   })
 //   .write();
 
-console.log(db.get("topic").find({ name: "lowdb", author: 1 }).value());
+// 5강. 조회
+// console.log(db.get("topic").find({ title: "lowdb", author: 1 }).value());
+
+// 6강. 수정
+db.get("topic").find({ id: 2 }).assign({ title: "MySQL & MariaDB" }).write();
